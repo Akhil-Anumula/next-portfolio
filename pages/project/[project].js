8a1fd2feb74719a73cf9b77b2fa15dc7a4a9e147
Akhil-Anumula <akhil.anumula23@gmail.com>
2022-12-09 17:06:19 +0530

    minor changes

--- pages/project/[project].js
+++ pages/project/[project].js
@@ -130,8 +130,8 @@ function projectDetails() {
           <p>Project</p>
           <h2>Overview</h2>
           <p className='pt-4'>{reqProject.description}</p>
-          <a href={reqProject.demo} target="_blank"><button className='px-8 py-2 mt-4 mr-8'>Demo</button></a>
-          <a href={reqProject.code} target="_blank"><button className='px-8 py-2 mt-4'>Code</button></a>
+          <a href={reqProject.demo} target="_blank" rel="noreferrer"><button className='px-8 py-2 mt-4 mr-8'>Demo</button></a>
+          <a href={reqProject.code} target="_blank" rel="noreferrer"><button className='px-8 py-2 mt-4'>Code</button></a>
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
           <div className="p-2">
